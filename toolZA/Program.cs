1d03e66213b62f86a90d3cd2fda6cedc7e14a481
Vitalii Mahlona <maglona2013@gmail.com>
2019-05-15 15:09:25 +0300

    finished beta

--- toolZA/Program.cs
+++ toolZA/Program.cs
@@ -14,7 +14,7 @@ namespace toolZA
         {
             Console.OutputEncoding = Encoding.UTF8;
             const string rootDirName = @"D:\DictionaryForFullStack\DictionaryForFullStack";
-            //string connectionString = ConfigurationManager.ConnectionStrings["LearningLanguages"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["LearningLanguages"].ConnectionString;
             
 
             if (Directory.Exists(rootDirName))
@@ -26,18 +26,39 @@ namespace toolZA
                 {
                     ConsoleWriteExcel(s);
 
+                    DirectoryInfo rootDirInfo = new DirectoryInfo(s);
+                    if (rootDirInfo.Name == "Test_Icons") continue;
+
                     string[] dirs = Directory.GetDirectories(s);
                     Console.WriteLine("SubCategories:");
 
                     foreach (string d in dirs)
                     {
                         DirectoryInfo dirInfo = new DirectoryInfo(d);
-                        if (dirInfo.Name == "pictures") continue;
-                        Console.WriteLine(dirInfo.Name);
+                        ConsoleWriteExcel(dirInfo.FullName);
 
-                        ConsoleWriteExcel(d);
+                        if (dirInfo.Name != "pictures")
+                        {
+                            string[] subDirs = Directory.GetDirectories(dirInfo.FullName);
+                            string picturesWordsDir = new DirectoryInfo(subDirs[0]).FullName;
+                            ConsoleWriteExcel(picturesWordsDir);
 
+                            Console.WriteLine("Pronounce: ");
+                            string pronounceWordsDir = new DirectoryInfo(subDirs[1]).FullName;
+                            string[] pronounceWordsSubDirs = Directory.GetDirectories(pronounceWordsDir);
+                            foreach (string p in pronounceWordsSubDirs)
+                            {
+                                DirectoryInfo subDirsPronounceInfo = new DirectoryInfo(p);
+                                Console.WriteLine(subDirsPronounceInfo.Name);
+                                string[] filesSubDirsPronounce = Directory.GetFiles(subDirsPronounceInfo.FullName);
 
+                                foreach (string k in filesSubDirsPronounce)
+                                {
+                                    FileInfo fileInf = new FileInfo(k);
+                                    Console.WriteLine(fileInf.Name);
+                                }
+                            }
+                        }
                     }
 
                     Console.WriteLine();
@@ -54,8 +75,8 @@ namespace toolZA
 
                 //////
                 //string file = Directory.GetFiles(rootDirName)[0];
-                //FileInfo fileInf = new FileInfo(file);
-                //var excel = new ExcelQueryFactory(fileInf.FullName);
+                //FileInfo fileInfo = new FileInfo(file);
+                //var excel = new ExcelQueryFactory(fileInfo.FullName);
                 //var worksheetList = excel.GetWorksheetNames().ToList();
                 //var categoriesRoot = from c in excel.Worksheet<CategoriesRoot>(worksheetList[0])
                 //                     select c;
@@ -68,8 +89,8 @@ namespace toolZA
                 //    {
                 //        if (!String.IsNullOrEmpty(i.Name))
                 //        {
-                //            //command.CommandText = $"INSERT INTO CategoriesRoot (Name, UA, RU, ENU, GER, CHI, POR, SPA, POL) VALUES ('{i.Name}',N'{i.UA}',N'{i.RU}','{i.ENU}','{i.GER}',N'{i.CHI}','{i.POR}','{i.SPA}','{i.POL}')";
-                //            command.CommandText = $"DELETE FROM CategoriesRoot WHERE Name='{i.Name}'";
+                //            command.CommandText = $"INSERT INTO Categories (name, parentId, picture) VALUES ('{i.Name}', '{null}', '{fileInfo.FullName}')";
+                //            //command.CommandText = $"DELETE FROM Categories WHERE name='{i.Name}'";
 
                 //            command.ExecuteNonQuery();
                 //        }
@@ -98,9 +119,31 @@ namespace toolZA
         public static void ConsoleWriteExcel(string nameDir)
         {
             DirectoryInfo RootDirInfo = new DirectoryInfo(nameDir);
-            if (RootDirInfo.Name == "pictures") return;
+            if (RootDirInfo.Name == "pictures")
+            {
+                Console.WriteLine("Pictures: ");
+                string[] filesPictures = Directory.GetFiles(RootDirInfo.FullName);
+                foreach (string s in filesPictures)
+                {
+                    FileInfo fileInf = new FileInfo(s);
+                    Console.WriteLine(fileInf.Name);
+                }
+                return;
+            }
+            if (RootDirInfo.Name == "Test_Icons")
+            {
+                string dirTestIcons = Directory.GetDirectories(RootDirInfo.FullName)[0];
+                string dirWhiteIcons = Directory.GetDirectories(dirTestIcons)[1];
+                string[] filesWhiteIcons = Directory.GetFiles(dirWhiteIcons);
+                foreach (string s in filesWhiteIcons)
+                {
+                    FileInfo fileInf = new FileInfo(s);
+                    if (fileInf.Name.StartsWith("Test"))
+                        Console.WriteLine(fileInf.Name);
+                }
+                return;
+            }
             Console.WriteLine(RootDirInfo.Name);
-
             string fileCategories = Directory.GetFiles(nameDir)[0];
             ExcelSelect(fileCategories);
 
